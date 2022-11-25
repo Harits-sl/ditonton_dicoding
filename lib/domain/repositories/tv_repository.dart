3e217ee792bb8d146a724fca3496c39f67ed5166
Harits-sl <harits.salsabil@gmail.com>
2022-11-26 05:33:35 +0700

    add usecase tv popular, top rated, and now playing

---
+++ lib/domain/repositories/tv_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/get_popular_tvs.dart';
+
+abstract class TvRepository {
+  Future<Either<Failure, List<Tv>>> getNowPlayingTvs();
+  Future<Either<Failure, List<Tv>>> getPopularTvs();
+  Future<Either<Failure, List<Tv>>> getTopRatedTvs();
+}
