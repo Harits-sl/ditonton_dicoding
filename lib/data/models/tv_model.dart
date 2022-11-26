6b18e1a360a8a3a763b4b16f6a9ef1de83ec14a7
Harits-sl <harits.salsabil@gmail.com>
2022-11-26 08:07:11 +0700

    add tv model

---
+++ lib/data/models/tv_model.dart
@@ -0,0 +1,90 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:ditonton/domain/entities/tv.dart';
+
+class TvModel extends Equatable {
+  TvModel({
+    required this.posterPath,
+    required this.popularity,
+    required this.id,
+    required this.backdropPath,
+    required this.voteAverage,
+    required this.overview,
+    required this.firstAirDate,
+    required this.genreIds,
+    required this.voteCount,
+    required this.name,
+    required this.originalName,
+  });
+
+  final String posterPath;
+  final double popularity;
+  final int id;
+  final String backdropPath;
+  final double voteAverage;
+  final String overview;
+  final String firstAirDate;
+  final List<int> genreIds;
+  final int voteCount;
+  final String name;
+  final String originalName;
+
+  factory TvModel.fromJson(Map<String, dynamic> json) => TvModel(
+        posterPath: json["poster_path"],
+        popularity: json["popularity"].toDouble(),
+        id: json["id"],
+        backdropPath: json["backdrop_path"],
+        voteAverage: json["vote_average"].toDouble(),
+        overview: json["overview"],
+        firstAirDate: json["first_air_date"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        voteCount: json["vote_count"],
+        name: json["name"],
+        originalName: json["original_name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "poster_path": posterPath,
+        "popularity": popularity,
+        "id": id,
+        "backdrop_path": backdropPath,
+        "vote_average": voteAverage,
+        "overview": overview,
+        "first_air_date": firstAirDate,
+        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+        "vote_count": voteCount,
+        "name": name,
+        "original_name": originalName,
+      };
+
+  Tv toEntity() => Tv(
+        backdropPath: backdropPath,
+        genreIds: genreIds,
+        id: id,
+        originalName: originalName,
+        overview: overview,
+        popularity: popularity,
+        posterPath: posterPath,
+        firstAirDate: firstAirDate,
+        name: name,
+        voteAverage: voteAverage,
+        voteCount: voteCount,
+      );
+
+  @override
+  List<Object> get props {
+    return [
+      posterPath,
+      popularity,
+      id,
+      backdropPath,
+      voteAverage,
+      overview,
+      firstAirDate,
+      genreIds,
+      voteCount,
+      name,
+      originalName,
+    ];
+  }
+}
--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,6 +1,7 @@
 import 'package:ditonton/data/datasources/db/database_helper.dart';
 import 'package:ditonton/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
+import 'package:ditonton/data/datasources/tv_remote_data_source.dart';
 import 'package:ditonton/domain/repositories/movie_repository.dart';
 import 'package:ditonton/domain/repositories/tv_repository.dart';
 import 'package:mockito/annotations.dart';
@@ -8,10 +9,11 @@ import 'package:http/http.dart' as http;
 
 @GenerateMocks([
   MovieRepository,
-  TvRepository,
   MovieRemoteDataSource,
   MovieLocalDataSource,
   DatabaseHelper,
+  TvRepository,
+  TvRemoteDataSource,
 ], customMocks: [
   MockSpec<http.Client>(as: #MockHttpClient)
 ])
