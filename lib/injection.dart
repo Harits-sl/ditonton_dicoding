a4caba4fbf90511b467051884fc2fa09fddf023b
Harits-sl <harits.salsabil@gmail.com>
2022-12-27 12:20:12 +0700

    add package

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,6 +1,5 @@
 import 'package:core/core.dart';
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart' as http;
 import 'package:movie/movie.dart';
 import 'package:search/search.dart';
 import 'package:tv/tv.dart';
